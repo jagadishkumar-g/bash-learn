0ed4dcf6c2313a618991187e261b46de5fbb439a
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-09-29 19:28:00 +0530

    added practice scripts

---
+++ practice scripts/direxists-auto.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOME_DIR="/home/ubuntu"
+
+# HOME_DIR="/Desktop/jagadish-repos"
+read -r -p "Enter the directory name which you want to create: " DIR_NAME
+
+cd "$HOME_DIR" || { echo "Home directory not found"; exit 1; }
+
+# cd "$HOME_DIR" || exit
+
+if [ -d "$DIR_EXIST" ]; then
+    echo "Folder already exists."
+else
+    mkdir "$DIR_NAME"
+    echo "Folder '$DIR_NAME' has been created."
+fi

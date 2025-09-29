58367b061bccceb73826e9c727d2a3c859acb80b
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-09-29 20:14:20 +0530

    added practice scripts

--- practice scripts/fileexist-auto.sh
+++ practice scripts/fileexist-auto.sh
@@ -5,12 +5,12 @@
 read -r -a -p "Enter file names separated by space: " FILES
 cd "$HOME_DIR" || { echo "Home directory not found."; exit 1; }
 
-for file in "${FILES[@]}";
+for file in $FILES;
 do
     if [ -f "$file" ]; then
         echo "$file File already exist"
     else
         touch "$file";
-        echo "created "$file". please check and let us know"
+        echo "created $file. please check and let us know"
     fi
 done

7b362821f03e4d4d382d61ed7478d510fc8ff000
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-09-29 20:05:31 +0530

    added practice scripts

--- practice scripts/fileexist-auto.sh
+++ practice scripts/fileexist-auto.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+HOME_DIR="$HOME"
+
 read -r -p "Enter file names separated by space: " FILES
-cd HOME_DIR || { echo "Home directory not found."; exit 1; }
+cd "$HOME_DIR" || { echo "Home directory not found."; exit 1; }
 
 for file in $FILES;
 do

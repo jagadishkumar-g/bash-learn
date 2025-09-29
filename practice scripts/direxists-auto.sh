61b3e177b954389bb424937f8ebcddfae2e618c2
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-09-29 19:43:17 +0530

    added practice scripts

--- practice scripts/direxists-auto.sh
+++ practice scripts/direxists-auto.sh
@@ -7,15 +7,21 @@ read -r -p "Enter the directory name which you want to create: " DIR_NAME
 
 cd "$HOME_DIR" || { echo "Home directory not found"; exit 1; }
 
-# cd "$HOME_DIR" || exit
 
-if [ -d "$DIR_NAME" ]; then
-    echo "Folder already exists."
-else
-    if mkdir "$DIR_NAME"; then
-        echo "Directory has been created successfully."
-    else
-        echo "Folder '$DIR_NAME' has been created."
-        exit 1
-    fi
-fi
+
+########## simple method #################
+
+mkdir -p "$DIR_NAME" && echo "Created $DIR_NAME and it's ready for use." >> folder_log.txt 
+
+
+########## using if statements ############################
+# if [ -d "$DIR_NAME" ]; then
+#     echo "Folder already exists."
+# else
+#     if mkdir "$DIR_NAME"; then
+#         echo "Directory has been created successfully."
+#     else
+#         echo "Folder '$DIR_NAME' has been created."
+#         exit 1
+#     fi
+# fi

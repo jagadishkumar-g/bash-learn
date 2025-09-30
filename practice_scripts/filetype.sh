0cdaad3434b47d1d97352fe702c9acef320b0fad
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-09-30 22:07:50 +0530

    for loop

--- practice_scripts/filetype.sh
+++ practice_scripts/filetype.sh
@@ -4,17 +4,21 @@ read -r -a RESOURCES -p "Enter multiple name by space: "
 
 for sources in "${RESOURCES[@]}"; do
     if [ -f "$HOME/$sources" ]; then
+        echo "========================"
         echo "FILE name: $sources" 
         echo "File size: $(stat -c %s "$HOME/$sources")"
         echo "Resource Type: FILE"
+        echo "========================"
         ((FILEFOUND++))
     elif [ -d "$HOME/$sources" ]; then
         echo "Directory name: $sources" 
         echo "Directory size: $(stat -c %s "$HOME/$sources")"
         echo "Resource Type: FILE"
         ((DIRFOUND++))
+        echo "========================"
     else
         echo "$sources not found"
+        echo "========================"
         ((NOTFOUND++))
     fi
 done

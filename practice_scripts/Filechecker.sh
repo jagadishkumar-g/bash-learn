c49b519a604b7d9cab468e94c2e7537f7163ef81
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-09-30 21:30:34 +0530

    for loop

--- practice_scripts/Filechecker.sh
+++ practice_scripts/Filechecker.sh
@@ -7,7 +7,15 @@ HOME_VAR="$HOME"
 for file in "${FILES[@]}"; do
     if [ -f "$HOME_VAR/$file" ]; then
         echo "File $file exists"
+        ((exist++))
     else
         echo "File $file does NOT exist"
+        ((missing++))
     fi
 done
+
+
+echo ""
+echo "Total files: ${#FILES[@]}"
+echo "EXISTING FILES: $exist"
+echo "MISSING FILES: $missing"

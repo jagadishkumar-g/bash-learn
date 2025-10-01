576f7562475cacac35f4ff7b1e2cfea6e067d151
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-01 20:22:25 +0530

    for loop

--- practice_scripts/guesswhile.sh
+++ practice_scripts/guesswhile.sh
@@ -5,12 +5,12 @@
 # Keep asking until they guess the correct number.
 # Print how many tries it took.
 
-read -r -p "Guess a number between 1 and 9: " GUESS
 secret=8
 GUESS=0
 attempts=0
 
 while true; do
+    read -r -p "Guess a number between 1 and 9: " GUESS
     ((attempts++))
     if [ "$GUESS" = $secret ]; then
         echo "You secret number is $secret"

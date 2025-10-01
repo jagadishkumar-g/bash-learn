5edfaf3caadc757ae75a9d7885e925bb3be09844
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-01 21:32:33 +0530

    for loop

---
+++ practice_scripts/functions/filecreatefunc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Asks the user to enter multiple names separated by spaces.
+# Asks whether each item should be a file or folder.
+# Checks if each name already exists.
+# Creates the file/folder if it doesn’t exist.
+# Logs all creations to a file called creation.log.
+
+# Function to create file and folders.
+
+create_item() {
+    local NAME=$1
+    local TYPE=$2
+
+    if [ -e "$HOME/$NAME" ]; then
+        echo "$NAME Already Exist"
+    elif [ "$TYPE" = "FILE" ]; then
+        touch $NAME && echo "File '$NAME' created." | tee -a creation.log
+    elif [ "$TYPE" = "Directory" ]; then
+        touch $NAME && echo "Directory '$NAME' created." | tee -a creation.log
+    else
+        echo "Invaild File Type, Please enter correct "File Type""
+    fi
+}
+
+read -r -p "Enter names separated by space: " NAME
+read -r -p "Do you want to create files or folders? (file/folder): " TYPE 
+
+for item in $NAME; do
+    create_item $item $TYPE
+done
+
+echo "All done! Check creation.log for details."

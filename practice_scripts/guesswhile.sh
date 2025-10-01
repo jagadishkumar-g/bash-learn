25a11800371c25218b4e5ba6b94cfe24dbc1f302
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-01 20:34:24 +0530

    for loop

--- practice_scripts/guesswhile.sh
+++ practice_scripts/guesswhile.sh
@@ -6,16 +6,15 @@
 # Print how many tries it took.
 
 secret=8
-river=0
-attempts=0
+guess=0
 
-while [ $river -ne $secret ]; do
-    read -r -p "Guess a number between 1 and 9: " river
+while [ $guess -ne $secret ]; do
+    read -r -p "Guess a number between 1 and 9: " guess
     ((attempts++))
-    if [ $river -ne $secret ]; then
+    if [ $guess -ne $secret ]; then
         echo "You secret number is $secret"
     fi
 done
 
 echo "Total Attempts: $attempts"
-echo "Total Guess Taken: $river"
+echo "Total Guess Taken: $guess"

81bf3b6d3a35d9274e09068efa77fc7f977c6fa5
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-01 21:05:03 +0530

    for loop

---
+++ practice_scripts/Loops/numberfind.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Task 1: Number Guess Game (Enhanced)
+# Enhance your previous “guess the number” script.
+# Requirements:
+# Only accept numbers as input.
+# Give hints: “Too high” or “Too low”.
+# Limit maximum attempts to 5.
+
+secret=8
+
+read -r -p "ENTER your number (1-9): " guess
+
+for attempts in {1..5}; do
+    if [[ ! "$guess" =~ ^[0-9]+$ ]]; then
+        echo "Enter the digit from 1-9"
+    fi
+
+    if [ "$guess" -eq "$secret" ]; then
+        echo "Secret number is : $secret."
+    elif [ "$guess" -lt "$secret" ]; then
+        echo "Too Low!"
+    else 
+        echo "Too High"
+    fi
+    
+    echo "Attempts left: $((5-attempts))" 
+done
+
+echo "Game Over! The secret number was $secret."

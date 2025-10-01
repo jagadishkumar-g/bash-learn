8b191b821a863116d3236edc948bb1140c705b9f
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-01 21:13:16 +0530

    for loop

--- practice_scripts/Loops/numberfind.sh
+++ practice_scripts/Loops/numberfind.sh
@@ -24,7 +24,7 @@ for attempts in {1..5}; do
         echo "Too Low!"
         echo "Attempts left: $((5-attempts))" 
     else 
-        echo "Congratulations! Secret number is: $secret"
+        echo "/nCongratulations! Secret number is: $secret"
         exit 0
     fi    
 done

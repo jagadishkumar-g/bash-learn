42289795a1f34900706523ad382581b5e23d76d6
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-08 20:11:03 +0530

    AWS

--- aws/aws-delete-iam-users.sh
+++ aws/aws-delete-iam-users.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # storing all username in the variable. 
-USERS_LIST=($(aws iam list-users --query 'Users[*].UserName' --output text))
+USERS_LIST=$(aws iam list-users --query 'Users[*].UserName' --output text)
 
 for user in "${USERS_LIST[@]}"; do
     if ! [[ "$user" = "admin-user" || "$user" = "ravi-sha" ]]; then
-        aws iam detach-user-policy --user-name "$user" --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
-        aws iam delete-login-profile --user-name "$user" || true
-        aws iam delete-user --user-name "$user"
-        echo "$user is deleted from IAM."
+
+    echo "Cleaning up $user"
+
+    POLICIES=$(aws iam list-attached-user-policies --user-name "$user" --query 'AttachedPolicies[*].PolicyArn' --output text) 
+    for p in $POLICIES; do
+        aws iam detach-user-policy --user-name "$user" --policy-arn "$p"
+    done
+
+    aws iam detach-user-policy --user-name "$user" --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
+    aws iam delete-login-profile --user-name "$user" || true
+    aws iam delete-user --user-name "$user"
+    echo "$user is deleted from IAM."
     else
         echo "$user is potential user, can't be deleted."
     fi

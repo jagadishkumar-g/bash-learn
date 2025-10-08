d3eae825d98f3786b2abb8d2fd6738c3d2c0bc2f
jagadishkumar-g <jagadishgangupalli.devops@gmail.com>
2025-10-08 20:02:52 +0530

    added aws folder

--- aws/aws-delete-iam-users.sh
+++ aws/aws-delete-iam-users.sh
@@ -8,6 +8,7 @@ for user in "${USERS_LIST[@]}"; do
         aws iam detach-user-policy --user-name "$user" --policy-arn arn:aws:iam::aws:policy/AdministratorAccess
         aws iam delete-login-profile --user-name "$user" || true
         aws iam delete-user --user-name "$user"
+        echo "$user is deleted from IAM."
     else
         echo "$user is potential user, can't be deleted."
     fi
